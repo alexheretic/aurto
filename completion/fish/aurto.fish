7add8aa48d60f8597324d4367b3898b65bdb8eb6
Alex Butler <alexheretic@gmail.com>
2018-06-04 16:16:42 +0100

    Add `aurto add` completion via aursearch/jq

--- completion/fish/aurto.fish
+++ completion/fish/aurto.fish
@@ -1,7 +1,19 @@
 ## Tab completion for aurto
 
+function __aurto_complete_add
+  set -l search (commandline -ct)
+  if [ (string length $search) -gt 2 ]
+    aursearch -nrP "^$search" \
+      | jq '.[].results[] | .Name' \
+      | cut -d'"' -f2
+  end
+end
+
 complete -c aurto -x -n '__fish_use_subcommand' -a 'add' -d 'Add & build aur packages by name'
 complete -c aurto -x -n '__fish_seen_subcommand_from add'
+if type -q aursearch; and type -q jq
+  complete -c aurto -x -n '__fish_seen_subcommand_from add' -a '(__aurto_complete_add)'
+end
 
 complete -c aurto -x -n '__fish_use_subcommand' -a 'addpkg' -d 'Add prebuilt aur package files'
 complete -c aurto -r -n '__fish_seen_subcommand_from addpkg'
